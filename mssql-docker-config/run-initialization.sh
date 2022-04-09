45dafd5af8cc6ce74a9447f404d92f85fc775072
Yakov Waimer <yakov.05@mail.ru>
2022-04-09 21:46:08 +0500

    Дополнил README.md

--- mssql-docker-config/run-initialization.sh
+++ mssql-docker-config/run-initialization.sh
@@ -2,5 +2,4 @@
 sleep 30s
 
 # Run the setup script to create the DB and the schema in the DB
-# Note: make sure that your password matches what is in the Dockerfile
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P PHxvf8TEKtrK -d master -i create-database.sql
