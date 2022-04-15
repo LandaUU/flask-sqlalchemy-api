760a264e131416372f976c28aa6266016a2ac422
Yakov Waimer <yakov.05@mail.ru>
2022-04-15 18:05:17 +0500

    Изменены файлы для запуска контейнеров

--- mssql-docker-config/run-initialization.sh
+++
@@ -1,5 +0,0 @@
-# Wait to be sure that SQL Server came up
-sleep 30s
-
-# Run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P PHxvf8TEKtrK -d master -i create-database.sql
